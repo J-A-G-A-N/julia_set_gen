389e6fa705d58081defd3aa3a92e503b2ef5ffd0
Jagan <jaganp5002@gmail.com>
2025-05-20 23:28:58 +0530

    Improved writePPM6

--- src/main.zig
+++ src/main.zig
@@ -2,29 +2,39 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const cwd = std.fs.cwd();
 const max_iter: u8 = 255;
+
 const julia_type = std.math.Complex(f32);
-const screenwidth: usize = 1920;
+const screenwidth: usize = 2048;
 const screenheight: usize = 1080;
+
 const inv_width = 1.0 / @as(f32, @floatFromInt(screenwidth));
 const inv_height = 1.0 / @as(f32, @floatFromInt(screenheight));
 const max_value1: f32 = 2;
+
 pub fn main() !void {
     const buf_len: usize = screenwidth * screenheight;
+    // Make frames directory
     try cwd.makePath("frames");
+
+    // Julia buff (Stores the brightness of each pixel)
     var juila_arr: [buf_len]u8 = undefined;
     const juila_buf = juila_arr[0..];
+
     var image_arr: [buf_len][3]u8 = undefined;
     const image_buf = image_arr[0..];
+
     const fps: usize = 60;
-    const seconds: usize = 30;
+    const seconds: usize = 60;
     const total_seconds: usize = seconds * fps;
+
     var i: usize = 0;
     var time: f32 = 0;
     const dt: f32 = 1.0 / @as(f32, @floatFromInt(fps));
 
-    var path_buf_arr: [1024]u8 = undefined;
+    var path_buf_arr: [2048]u8 = undefined;
     const path_buf = path_buf_arr[0..];
     const start_time = @as(u64, @intCast(std.time.nanoTimestamp()));
+
     //const c = julia_type.init(-0.8, 0.156);
     while (i < total_seconds) : (i += 1) {
         const c = julia_type.init(
@@ -42,6 +52,7 @@ pub fn main() !void {
         try printProgressBar(i, total_seconds, start_time);
     }
 }
+
 fn iterationToRGB(iter: u8, max_iterations: u8) [3]u8 {
     const t = @as(f32, @floatFromInt(iter)) / @as(f32, @floatFromInt(max_iterations));
     const r: u8 = @intFromFloat(9.0 * (1.0 - t) * t * t * t * 255.0);
@@ -84,17 +95,17 @@ fn printProgressBar(current: usize, total: usize, start_time: u64) !void {
         speed,
     });
 }
-
 fn writePPm6(file_name: []const u8, buf: []const [3]u8, width: usize, height: usize) !void {
     const file = try cwd.createFile(file_name, .{ .truncate = true });
     defer file.close();
     const writer = file.writer();
 
     // Write File Header
-    try writer.print("P6\n{} {}\n{}\n", .{ width, height, max_iter });
-    for (buf) |pixel| {
-        try writer.writeAll(&pixel);
-    }
+    try writer.print("P6\n{} {}\n{}\n", .{ width, height, 255 });
+
+    // write the image
+    const raw_bytes = @as([*]const u8, @ptrCast(buf.ptr))[0 .. buf.len * 3];
+    try writer.writeAll(raw_bytes);
 }
 
 fn calculateJulia(buf: []u8, width: usize, height: usize, c: julia_type, max_iterations: usize, max_value: f32) void {
